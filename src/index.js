5a53728f8e37a5e826de5f432bf811fbd71fb885
Vivek maurya <vivek563maurya@gmail.com>
2023-09-17 17:00:35 +0530

    "Sign In with Google"

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<GoogleOAuthProvider clientId="provide you crediential id here">
     <App />
+
+</GoogleOAuthProvider>
   </React.StrictMode>
 );
 
